72a84980259348124ebd6533c4bf30370c9e5a77
Chaeyoon Kim <cy6kimm@gmail.com>
2024-05-23 02:06:25 +0900

    initial commit

---
+++ src/main/java/com/example/healthbuddy/HealthBuddyApplication.java
@@ -0,0 +1,13 @@
+package com.example.healthbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthBuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthBuddyApplication.class, args);
+	}
+
+}
